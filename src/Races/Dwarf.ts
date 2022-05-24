dd29eea3cfad21e3bbb18f41ca40628444ed2df6
melobrunoti <melobrunoti@gmail.com>
2022-05-24 16:02:42 -0300

    req 02

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,15 @@
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  private static _instancesCreated = 0;
+  private _maxLifePoints = 80;
+
+  static createdRacesInstances(): number {
+    this._instancesCreated += 1;
+    return this._instancesCreated;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
---
+++ src/Races/Elf.ts
@@ -0,0 +1,15 @@
+import Race from './Race';
+
+export default class Elf extends Race {
+  private static _instancesCreated = 0;
+  private _maxLifePoints = 99;
+
+  static createdRacesInstances(): number {
+    this._instancesCreated += 1;
+    return this._instancesCreated;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
